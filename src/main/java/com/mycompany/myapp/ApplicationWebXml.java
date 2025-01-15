965c55355fff21f246438895aed0493526823230
p1yushsharma <shrmapiyush5@gmail.com>
2025-01-15 14:08:32 +0530

    u1.8

--- src/main/java/com/mycompany/myapp/ApplicationWebXml.java
+++ src/main/java/com/mycompany/myapp/ApplicationWebXml.java
@@ -2,7 +2,7 @@ package com.mycompany.myapp;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import tech.jhipster.config.DefaultProfileUtil;
+
 
 /**
  * This is a helper Java class that provides an alternative to creating a {@code web.xml}.
